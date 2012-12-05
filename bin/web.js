27d91d54b6e01bc969ca51b76fff617fa7a95054
abrkn <a@abrkn.com>
2012-12-05 10:47:11 +0100

    dont crash on uncaught exceptions

--- bin/web.js
+++ bin/web.js
@@ -2,4 +2,10 @@ process.env.DEBUG || (process.env.DEBUG = '.*');
 
 console.log('starting web app');
 
+process.on('uncaughtException', function(err) {
+    console.error('uncaught exception in process')
+    console.error(err)
+    console.error(err.stack)
+})
+
 var app = new (require('../lib/server/App'))(process.env.PORT || 4010);
