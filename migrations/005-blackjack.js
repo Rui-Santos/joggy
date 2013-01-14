b0ab67a7cbe3e3192d50a3e00feeb845aac88a40
abrkn <a@abrkn.com>
2013-01-14 16:56:04 +0100

    add bj button and ability to disable it in config

---
+++ migrations/005-blackjack.js
@@ -0,0 +1,36 @@
+db.machines.insert({
+        "_id" : "bj1",
+        "game" : "bj",
+        "rules" : {
+                "doubleAfterSplit" : true,
+                "hitSplitAces" : true,
+                "reSplitAces" : true,
+                "maxSplits" : 2,
+                "hitSoft17" : false,
+                "insurance" : false,
+                "surrender" : false,
+                "decks" : 8,
+                "decisionTime" : 10,
+                "minBet" : 200000,
+                "maxBet" : 200000
+        }
+});
+
+db.machines.insert({
+        "_id" : "bj2",
+        "game" : "bj",
+        "rules" : {
+                "doubleAfterSplit" : true,
+                "hitSplitAces" : true,
+                "reSplitAces" : true,
+                "maxSplits" : 2,
+                "hitSoft17" : false,
+                "insurance" : false,
+                "surrender" : false,
+                "decks" : 8,
+                "decisionTime" : 10,
+                "minBet" : 200000,
+                "maxBet" : 200000
+        }
+});
+
